aaa637cc92ca7cff17027d67bac1c1d7fdbdd2c5
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-08-04 16:47:02 -0500

    Button back

--- src/components/ButtonBack.jsx
+++ src/components/ButtonBack.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { useHistory, useLocation } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import { ButonBack, ContentButtonBack, IIcon } from './button-back/ButtonBack'
 
 const ButtonBack = () => {
     const history = useHistory()
-    const location = useLocation()
-    console.log(history)
-    console.log(location)
+
     return (
         <ContentButtonBack>
-            <ButonBack>
+            <ButonBack onClick={history.goBack}>
                 <IIcon className="fas fa-less-than"></IIcon>
             </ButonBack>
         </ContentButtonBack>
