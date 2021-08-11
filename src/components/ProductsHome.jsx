f9ba6b8215512f37e9fe82912bd224f6836a78fd
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-08-11 11:07:30 -0500

    Loader Home

--- src/components/ProductsHome.jsx
+++ src/components/ProductsHome.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import CardProduct from './CardProduct'
+import Loader from './Loader'
 import { ContainerProductsHome } from './products-home-styled/ProductsHomeStyled'
 
 const ProductsHome = ({ modeMyProfile, dataProducts }) => {
     return (
         <ContainerProductsHome>
+            {console.log(dataProducts.length !== 0)}
             {
+                dataProducts.length !== 0 ?
                 dataProducts.map((product, index) => (
                     <CardProduct 
                         key={index} 
@@ -13,6 +16,7 @@ const ProductsHome = ({ modeMyProfile, dataProducts }) => {
                         product={product}
                     />
                 ))
+                : <Loader />
             }
         </ContainerProductsHome>
     )
